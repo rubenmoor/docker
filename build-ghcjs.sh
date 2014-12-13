70c880b0a54e85dc80e1b26ffa07ccb637414a0f
Ruben Moor <ruben.moor@gmail.com>
2014-12-13 20:43:33 +0100

    added ghcjs

---
+++ build-ghcjs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# exit on failure
+set -e
+
+# build images
+sudo docker build -t "rubenmoor/ghc-base:latest" `dirname "$0"`/ghc-base-image
+sudo docker build -t "rubenmoor/ghc:latest" `dirname "$0"`/ghc-image
+sudo docker build -t "rubenmoor/ghcjs:latest" `dirname "$0"`/ghcjs-image
